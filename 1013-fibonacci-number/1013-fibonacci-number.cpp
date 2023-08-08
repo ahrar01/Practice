5d7876b2d61e16178aeaf26e116826b5515d2836
Ahraar Alam <ahraralam2012@gmail.com>
2023-08-09 01:42:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int helper(int n, vector<int> &dp){
-        if(n<=1)return n;
-        if(dp[n]!=-1) return dp[n];
-
-        return dp[n] = helper(n-1,dp)+helper(n-2,dp);
-    }
+   
     int fib(int n) {
-        vector<int> dp(n+1,-1);
-        return helper(n,dp);
+       int a =0 , b=1;
+       for(int i=2;i<=n;i++){
+           int curr = a+b;
+           a = b;
+           b = curr;
+       }
+       return b;
     }
 };
